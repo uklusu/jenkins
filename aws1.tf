1fbda27c0e00354c144bbfca6cfe0550ccd35b6e
viktor <uklusu@gmail.com>
2022-02-04 13:37:03 +0200

    add_docker14

--- aws1.tf
+++ aws1.tf
@@ -36,7 +36,7 @@ echo \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
-
+sudo snap install docker
 EOF
 
 }
