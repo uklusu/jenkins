c3cda6885a1fabfa86e2a0154b18c1353b03808e
viktor <uklusu@gmail.com>
2022-02-06 01:35:09 +0200

    ping30

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 for server in $(cat /home/ubuntu/ip)
 do
-  if ssh -o StrictHostKeyChecking=no -i /home/ubuntu/id_rsa ubuntu@$server  "true"
+  if ssh -o  StrictHostKeyChecking=no -i /home/ubuntu/id_rsa ubuntu@$server  "true"
   then
     echo "Server $server: OK"
   else echo "ZA WARUDO" ; sleep 2m
