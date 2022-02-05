c5a224312a3ded3d44d24fc93ab5eb0b24fd31ca
viktor <uklusu@gmail.com>
2022-02-05 23:49:40 +0200

    ping14

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 for server in $(cat /home/ubuntu/ip)
 do
-  if ssh $server "true"
+  if ssh -o StrictHostKeyChecking=no -i /home/ubuntu/id_rsa $server "true"
   then
     echo "Server $server: OK"
   else sleep 2m
