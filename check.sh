836bbcd84a80d77fc4526a13a6edc49a5bdec684
viktor <uklusu@gmail.com>
2022-02-05 22:09:22 +0200

    opt21

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
- ssh -o - StrictHostKeyChecking=no -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
+ ssh -o  StrictHostKeyChecking=no -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
 while test $? -gt 0
 do
 echo "ZA WARUDO"
 sleep 2m
 
-ssh -o - StrictHostKeyChecking=no -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
+ssh -o  StrictHostKeyChecking=no -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
 done
