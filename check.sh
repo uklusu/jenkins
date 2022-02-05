f47a936b50cd1450ae22f51ea83a26f9393c1424
viktor <uklusu@gmail.com>
2022-02-05 21:50:21 +0200

    opt17

--- check.sh
+++ check.sh
@@ -1,9 +1,9 @@
 IP_ADD=cat /home/ubuntu/ip
- ssh -t -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
+ ssh -tt -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
 while test $? -gt 0
 do
 echo "ZA WARUDO"
 sleep 2m
 
- ssh -t -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
+ ssh -tt -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
 done
