754b99059038670afd0472ce37095d33f7d54ba9
viktor <uklusu@gmail.com>
2022-02-05 22:07:42 +0200

    opt20

--- check.sh
+++ check.sh
@@ -1,10 +1,8 @@
-sudo chmod 777 /home/ubuntu/ip
-IP_ADD= cat /home/ubuntu/ip
- ssh -o -tt StrictHostKeyChecking=no -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
+ ssh -o - StrictHostKeyChecking=no -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
 while test $? -gt 0
 do
 echo "ZA WARUDO"
 sleep 2m
 
-ssh -o -tt StrictHostKeyChecking=no -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
+ssh -o - StrictHostKeyChecking=no -i /home/ubuntu/id_rsa  ubuntu@$IP_ADD
 done
