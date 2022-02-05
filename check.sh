d1bf5f792830d7fff416e85ade06dcd3659fa4d2
viktor <uklusu@gmail.com>
2022-02-05 23:47:41 +0200

    ping13

--- check.sh
+++ check.sh
@@ -1,9 +1,8 @@
-ip_ad=$(cat /home/ubuntu/ip )
-ping -n 1 $ip_ad
-while test $? -gt 0
+for server in $(cat /home/ubuntu/ip)
 do
-echo "ZA WARUDO"
-sleep 2m
-
-ping -n 1 $ip_ad
+  if ssh $server "true"
+  then
+    echo "Server $server: OK"
+  else sleep 2m
+  fi
 done
