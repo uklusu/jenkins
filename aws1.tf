2437d2242a01a24acdcdaf48cac6d11524d39165
viktor <uklusu@gmail.com>
2022-02-05 23:12:19 +0200

    ping1

--- aws1.tf
+++ aws1.tf
@@ -49,14 +49,16 @@ resource "aws_security_group" "webserver" {
   name        = "websec"
   description = "dyn_security"
   egress {
-    from_port = -1
-    to_port   = -1
-    protocol  = "icmp"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = -1
-    to_port   = -1
-    protocol  = "icmp"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
